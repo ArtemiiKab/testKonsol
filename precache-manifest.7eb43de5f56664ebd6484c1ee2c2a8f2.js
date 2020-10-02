cec91b49fb75db83c0629b62628baa728b815837
ArtemiiKab <artemiikab@gmail.com>
2020-10-02 10:21:08 +0300

    Updates

---
+++ precache-manifest.7eb43de5f56664ebd6484c1ee2c2a8f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee7779758ffab82f0b0f7c827465dbd5",
+    "url": "/testKonsol/index.html"
+  },
+  {
+    "revision": "d2d97b8e3d2c2e894a82",
+    "url": "/testKonsol/static/css/main.ca7c007a.chunk.css"
+  },
+  {
+    "revision": "bc94f42090fecfa61311",
+    "url": "/testKonsol/static/js/2.3bf368ee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/testKonsol/static/js/2.3bf368ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d97b8e3d2c2e894a82",
+    "url": "/testKonsol/static/js/main.5e0ef5f5.chunk.js"
+  },
+  {
+    "revision": "c07bafcf06b3da24a492",
+    "url": "/testKonsol/static/js/runtime-main.155178f1.js"
+  }
+]);
